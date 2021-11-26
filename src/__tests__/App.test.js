80b0df8f4cc8d5066a78e4b8db919750ca264b7a
Thomas Lamarche <glims@wanadoo.fr>
2021-11-26 13:49:43 +0100

    cleaning

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from '../App';
-import userEvent from '@testing-library/user-event';
 
 describe('Testing App() Component', () => {
   const { ResizeObserver } = window;
